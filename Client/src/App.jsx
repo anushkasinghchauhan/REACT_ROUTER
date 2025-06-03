c4f8b73c7fcf07ede9f660eefb8f958acf30970d
Anushka Singh <anushka.singh.civ22@itbhu.ac.in>
2025-06-03 16:40:51 +0530

    1st commit

---
+++ Client/src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
+import Footer from "./components/Footer/Footer";
+import Contact from "./components/Contact/Contact";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <Home />
+      <Contact />
+      <Footer />
+    </>
+  );
+}
+
+export default App;
